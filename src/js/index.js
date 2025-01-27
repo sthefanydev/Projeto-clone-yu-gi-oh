ad90846ac16758cb7615bae827b984249a71a588
sthefanydev <sthefanycosta001@gmail.com>
2025-01-26 23:04:03 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,20 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+    
+    // virar o cartao
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
+
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
 
